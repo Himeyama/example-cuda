698e7cd7047ed8301a629ae8aca765014ca87c62
Murata Mitsuharu <hikari.photon+dev@gmail.com>
2021-10-24 23:34:06 +0900

    コメント

--- test00.cu
+++ test00.cu
@@ -8,24 +8,37 @@ void hello_world(float* a){
 }
 
 int main(void){
+    // 配列の大きさ
     int size = 256;
+
+    // 配列のバイト数
     int n = size * sizeof(float);
+
+    // 配列メモリーと GPU メモリーのポインタ宣言
     float *a, *g;
 
+    // メモリー確保
     a = (float*)malloc(n);
     cudaMalloc((void**)&g, n);
     
+    // カーネル(関数)呼び出し
     hello_world<<<1, 256>>>(g);
+
+    // 待機
     cudaDeviceSynchronize();
     
+    // GPU メモリーをホストメモリーにコピー
     cudaMemcpy(a, g, n, cudaMemcpyDeviceToHost);
 
+    // 配列の中身を表示
     for(int i = 0; i < size; i++)
         printf("%f\n", a[i]);
 
+    // メモリー開放
     free(a);
     cudaFree(g);
 
+    // 割り当てを破壊し状態をリセット
     cudaDeviceReset();
 
     return 0;
